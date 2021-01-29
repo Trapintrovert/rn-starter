640119cdaa039f1a3a983c38ef484fc4f6cbba7d
trapintrovert <mcneo062@gmail.com>
2021-01-29 15:44:35 +0100

    Layout for RN

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const BoxScreen = () => {
+    return (
+        <View style={styles.viewStyle}>
+            <Text style={styles.textOneStyle}>Child 1</Text>
+            <Text style={styles.textTwoStyle}>Child 2</Text>
+            <Text style={styles.textThreeStyle}>Child 3</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+        height: 200,
+        alignItems: 'stretch'
+    
+    },
+    textOneStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        
+    },
+    textTwoStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        fontSize: 18,
+        position: 'absolute',
+        ...StyleSheet.absoluteFillObject
+
+    },
+    textThreeStyle: {
+        borderWidth: 3,
+        borderColor: 'red',
+        
+    }
+})
+
+export default BoxScreen
+
+
